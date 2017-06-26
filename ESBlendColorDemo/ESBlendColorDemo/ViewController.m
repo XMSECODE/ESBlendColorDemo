840f31f89970ecc4ed57cb818bcbd3a6bea93fc2
xiang <850794895@qq.com>
2017-06-26 17:56:01 +0800

    创建工程模板

---
+++ ESBlendColorDemo/ESBlendColorDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ESBlendColorDemo
+//
+//  Created by xiang on 2017/6/26.
+//  Copyright © 2017年 xiang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
